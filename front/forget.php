b5179fff2791ff0f3178d228022ba8a08e744499
unknown <eunice911qq@gmail.com>
2022-02-21 11:22:00 +0800

    完成忘記密碼及查詢密碼功能

--- front/forget.php
+++ front/forget.php
@@ -1,7 +1,7 @@
 <fieldset>
     <legend>忘記密碼</legend>
     <div>請輸入信箱以查詢密碼</div>
-    <div><input type="text" name="acc" id="acc"></div>
+    <div><input type="text" name="email" id="email"></div>
     <div id="result"></div>
     <div>
         <button onclick='find()'>尋找</button>
@@ -12,7 +12,7 @@
 <script>
     function find(){
         $.post("api/find_pw.php",{email:$("#email").val()},(result)=>{
-            $("#result").text(result)
+            $("#result").text(result) //將撈到的使用者密碼顯示出來
         })
     }
 </script>
